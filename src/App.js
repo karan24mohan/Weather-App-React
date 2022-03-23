03aaa19ad6f4b4bdb3d6bbbfc1d78f410b0e0c50
Karan <karanmohan2410@gmail.com>
2022-03-23 22:20:31 +0530

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,67 @@
 import "./App.css";
-import SearchWeather from "./Components/SearchWeather";
-import React from "react";
+import React, { useState } from "react";
 
+console.log(process.env);
 function App() {
+  const [weatherData, setWeatherData] = useState([{}]);
+  const [city, setCity] = useState("");
+
+  const getWeather = (event) => {
+    if (event.key === "Enter") {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_API_KEY}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setWeatherData(data);
+          setCity("");
+        });
+    }
+  };
   return (
-    <div className="App">
-      <SearchWeather />
+    <div className="container">
+      <div className="card bg-dark text-white">
+        <img
+          src="https://i.pinimg.com/564x/71/f0/ae/71f0ae5434b33a8a8e2bb4c45df1dd2a.jpg"
+          className="card-img"
+          alt="..."
+        />
+        <div className="card-img-overlay">
+          <input
+            type="text"
+            className="input"
+            placeholder="Enter City..."
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            onKeyPress={getWeather}
+          />
+          {typeof weatherData.main === "undefined" ? (
+            <p>
+              Welcome to the weather app! Enter in a city to get the weather
+              of...
+            </p>
+          ) : (
+            <div>
+              <h5 className="card-title city">{weatherData.name}</h5>
+
+              <p className="temp">
+                {(weatherData.main.temp - 273.15).toFixed(2)} &deg;C
+              </p>
+              <div className="temp-info">
+                <p className="temp-diff">
+                  H {(weatherData.main.temp_max - 273.15).toFixed(2)} &deg;C
+                </p>
+                <p className="temp-diff">
+                  L {(weatherData.main.temp_min - 273.15).toFixed(2)} &deg;C
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {weatherData.cod === "404" ? <p>City not found</p> : <></>}
     </div>
   );
 }
-
 export default App;
